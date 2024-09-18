a96f40c87278f2148be3321da367bc2ea748cb7c
Frederick Otu-Afro <frederickotuafro@gmail.com>
2024-09-18 13:43:07 +0000

    modal components created for close button etc..

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,17 +1,41 @@
 <template>
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
-            <RouterLink>
-                <div class="flex items-center gap-3 flex-1">
+            <RouterLink :to="{name : 'home'}">
+                <div class="flex items-center gap-3">
                     <i class="fa-solid fa-sun text-2xl"></i>
                     <p class="text-2xl">The Local Weather</p>
                 </div>
-            </RouterLink>
+            </RouterLink >
+
+            <div class="flex gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary 
+                duration-150 cursor-pointer " @click="toggleModal"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary 
+                duration-150 cursor-pointer"></i>
+            </div>
+
+            <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+                <div class="text-black">
+                    <h1 class="text-2xl mb-1">About:</h1>
+                    <p>The Local Weather allows you to track the current
+                        and future weather of cities of your choosing
+                    </p><br>
+                    <h2 class="text-2xl">How It Works:</h2>
+                </div>
+            </BaseModal>
         </nav>
     </header>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+    import { RouterLink } from 'vue-router';
+    import BaseModal from './BaseModal.vue'
 
+    const modalActive = ref(null);
+    const toggleModal = () => {
+        modalActive.value = !modalActive.value;
+    };
 </script>
 
