525762ff43cac50ceaa5518890b31203ac4d0c22
Frederick Otu-Afro <frederickotuafro@gmail.com>
2024-09-19 08:42:39 +0000

    chart, add and delete city added

---
+++ src/components/CityList.vue
@@ -0,0 +1,48 @@
+<template>
+    <div v-for="city in savedCities" :key="city.id">
+        <CityCard :city="city" @click="gotToCityView(city)"/>
+    </div>
+
+    <p v-if="savedCities.length === 0">
+        No locations added. To start tracking a location, search in the field above.
+    </p>
+</template>
+
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+import CityCard from "./CityCard.vue";
+import { useRouter } from "vue-router";
+
+const savedCities = ref([]);
+const getCities = async () => {
+    if (localStorage.getItem('savedCities')){
+        savedCities.value = JSON.parse(
+            localStorage.getItem('savedCities')
+        );
+
+        const requests = []
+        savedCities.value.forEach((city) => {
+            requests.push(
+                axios.get('https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=c05bc543e96932962cbe546a010c81c4&units=imperial')
+            )    
+            });
+            const weather = await Promise.all(requests);
+
+            weatherData.forEach((value,index) => {
+                savedCities.value[index].weather = value.data;
+            });
+    }
+};
+await getCities();
+
+const router = useRouter();
+const gotToCityView = (city) => {
+    router.push({
+        name: 'cityView',
+        params: {state: city.state, city: city.city},
+        query: {id: city.id, lat: city.coords.lat, lng: city.coords.lng},
+    });
+};
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,25 @@
       @input="getSearchResults" v-model="searchQuery" class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none 
       focus:shadow-[0px_1px_0_0_#004E71]"/>
       <ul v-if="mapboxSearchResults" class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+        <p v-if="searchError">Sorry, something went wrong</p>
+        <p v-if="!serverError && mapboxSearchResults === 0">
+          No results match your query; try a different term
+        </p>
+        <template v-else>
+        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" 
+        class="py-2 cursor-pointer" @click="previewCity(searchResult)">
           {{ searchResult.place_name }}
         </li>
+      </template>
       </ul>
+    </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList/>
+        <template #fallback>
+          <p>loading...</p>
+        </template>
+      </Suspense>
     </div>
   </main>
 </template>
@@ -16,22 +31,43 @@
 <script setup>
   import axios from 'axios';
   import { ref } from 'vue'
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+  const previewCity = () => {
+    const [city, state] = searchResult.place_name.split(",");
+    router.push({ 
+      name: 'cityView', 
+      params: { state: state.ReplaceAll(" ", ""), city: city },
+      query: {
+        lat: searchResult.geometry.coordinates[1],
+        lng: searchResult.geometry.coordinates[0],
+        preview: true,
+      }
+    });
+  }
 
   const mapboxAPIKey = 'pk.eyJ1IjoiYW5uYW5mcmVkZHkiLCJhIjoiY20xN3ljMDB2MHRuaTJxcXpnMTgydjhwZiJ9.hJ3qKVy3o2sfou7KcGbccA'
 
   const searchQuery = ref("")
   const queryTimeout = ref(null)
   const mapboxSearchResults = ref(null);
+  const searchError = ref(null)
 
   const getSearchResults = () => {
     clearTimeout(queryTimeout.value);
     queryTimeout.value = setTimeout(async () => {
       if (searchQuery !== ""){
-        const result = await axios.get('https://api.mapbox.com/search/geocode/v6/forward?q=${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place');
+        try {
+          const result = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place');
         mapboxSearchResults.value = result.data.features;
+        } catch {
+          searchError.value = true;
+        }
+        return;
       }
       mapboxSearchResults.value = null;
-      return;
+      
     }, 300);
   }
 </script>
